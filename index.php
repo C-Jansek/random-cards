afa335b8599f91fd7642bd10f9996ee534a61eb0
C-Jansek <49492561+C-Jansek@users.noreply.github.com>
2022-03-20 20:01:24 +0100

    Abstract card selection and include boilerplate html

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php
+
 $cards = [
     'Elke overtreding van de verdedigende partij is een vrije bal.',
     'Er is geen verdedigd.',
     'Schieten na snijden is toegestaan'
 ];
 
-$selectedCard = $cards[array_rand($cards)];
+function getRandomCard($cards)
+{
+    return $cards[array_rand($cards)];
+}
 ?>
 
-<h1><?php echo $selectedCard ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random referee card</title>
+</head>
+
+<body>
+    <h1><?php echo getRandomCard($cards) ?></h1>
+</body>
+
+</html>
